30c8abb7bf6de5f5d86d265968f5aeb675743ae4
Frank <eggen@emden.net>
2019-02-19 17:26:09 +0100

    Revert "Never allow neverallows"

--- build/core/config.mk
+++ build/core/config.mk
@@ -22,5 +22,10 @@ FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/rr/build/core/apichec
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/rr/build/core/system_rro.mk
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for QCOM targets
 include $(TOPDIR)vendor/rr/build/core/qcom_target.mk
